5d1d37d5a0be6d0aa2bbc2726711867755fa4110
Martin Nikl <martin.nikl@SAHOLDING.SA.PRIV>
2017-02-23 15:54:28 +0100

    pridani reactu

---
+++ react/webpack.config.js
@@ -0,0 +1,98 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractText = require('extract-text-webpack-plugin');
+
+module.exports = function (env) {
+
+  var project = {
+    NAME: 'REACT',
+    env: env.prod ? 'prod' : 'dev',
+    jsBase: './app/',
+    cssBase: './src/css/'
+  }
+
+  var suffix = project.env === 'prod' ? '----------------------->' : '------------------------------>'
+  console.log('<---------------------------------------------------------------------->')
+  console.log('<--------------------------- ' + project.NAME + ' ------------------------------>')
+  console.log('<----------------------- ' + project.env + ' environment ' + suffix)
+  console.log('<------------------- the ultimate webpack experience ------------------>')
+  console.log('<---------------------------------------------------------------------->')
+
+  var config = {
+    entry: {
+      'global': project.jsBase + 'main.js'
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].js'
+    },
+    devtool: project.env === 'prod' ? 'source-map' : 'eval-source-map', // viz.: https://webpack.js.org/configuration/devtool/
+    plugins: [
+      new ExtractText({
+        filename: 'styles.min.css',
+        disable: false,
+        allChunks: true
+      })
+    ],
+    resolve: {
+      extensions: ['.js', '.es6', '.jsx', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: path.join(__dirname, '/src/js'),
+          exclude: /node_modules/,
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015'],
+            plugins: ["rect-html-attrs", "transform-runtime"]
+          }
+        },
+        {
+          test: /\.(scss|sass)$/,
+          use: ExtractText.extract({
+            "fallback": "style-loader",
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: project.env === 'prod' ? true : false
+                }
+              },
+              'sass-loader'
+            ]
+          })
+        },
+        {
+          test: /\.(png|jpg|svg)$/,
+          use: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: 'url-loader?limit=10000&minetype=application/font-woff'
+        },
+        {
+          test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: 'file-loader'
+        }
+      ]
+    }
+  };
+
+  if(project.env === 'prod') {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        // source-map musi byt definovano i zde, jinak sse source mapy nebudou generovat (nebo jen pro css)
+        // viz.: http://stackoverflow.com/questions/30870830/how-do-i-generate-sourcemaps-when-using-babel-and-webpack
+        sourceMap: true
+      })
+    )
+  }
+
+  return config;
+}
--- webpack/webpack.config.babel.js
+++ webpack/webpack.config.babel.js
@@ -1,39 +1,22 @@
-import webpack from 'webpack';
-import path from 'path';
+var path = require('path')
 
-export default {
-  entry: ['./app/main.js'],
-  output: {
-    filename: 'bundle.js',
-    path: './dist',
-    // path: path.join(__dirname, 'build'),
+module.exports = function(env) {
+  return {
+    entry: ['./app/main.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      // path: './dist',
+      pathinfo: !env.prod
+    },
+    // context: '../egghead',
+    devtool: env.prod ? 'source-map' : 'eval',
+    // bail: path.resolve(__dirname, 'dist'),
+    // module: {
+    //   // loaders: {
+    //   //   {test: /\.js$/, loader: 'babel!esllint', exlude: /node_modules/},
+    //   //   {test: /\.css$/, loader: 'style!css'}
+    //   // }
+    // }
   }
 }
-
-// var path = require('path');
-// var webpack = require('webpack');
-//
-// module.exports = (function (env) {
-//   console.log('webpack started in ' + env + ' environment');
-//
-//   var config = {
-//     entry: {
-//       'global': './app/main.js'
-//     },
-//     output: {
-//       path: path.join(__dirname, '/dist'),
-//       filename: 'test'
-//     },
-//     module: {
-//       loaders: [{
-//         test: /\.js$/,
-//         loader: 'babel',
-//         include: path.join(__dirname, '/src/js'),
-//         exclude: /node_modules/
-//       }]
-//     }
-//   };
-//
-//   return config;
-//
-// })();
